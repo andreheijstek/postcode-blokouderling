ad831680445f97c2524a114fd6610fb6e274ac51
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-01-04 11:53:04 +0100

    Simple form created with javascript connected - just displaying an alert

---
+++ lookop_postcode.js
@@ -0,0 +1,10 @@
+/**
+ * Created by andreheijstek on 04-01-16.
+ */
+function lookup_postcode() {
+    if (!(postcode = "2805CZ")) {
+        return "Unknown"
+    } else {
+        return "Anne Bosman"
+    }
+}
