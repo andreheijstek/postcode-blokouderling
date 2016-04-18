19c9ac066f3bb4845588833b9b9844ca4183ed84
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-04-18 12:06:10 +0200

    Add cucumber test

--- features/step_definitions/postal_code.rb
+++ features/step_definitions/postal_code.rb
@@ -2,13 +2,15 @@ require 'capybara'
 
 Given(/^I live in this postal code area (\d{4}[a-zA-Z]{2})$/) do |postalcode|
   @postalcode = postalcode
+  visit "http://sintjansgemeente.nl/contact/wijkouderling/"
+  fill_in('postcode_id', :with => @postalcode)
 end
 
 When(/^I search for my blokouderling$/) do
-  visit "http://sintjansgemeente.nl/contact/wijkouderling/"
-  click(page.find(:xpath, '//span[contains(., "Contact")]'))
+  click_on('Vind uw blokouderling')
 end
 
 Then(/^the "([^"]*)" and "([^"]*)" should be shown$/) do |blokouderling, email|
-  pending # Write code here that turns the phrase above into concrete actions
+  element = find(:id, 'blokouderling_id')
+  expect(page).to have_content "Ouderling: #{blokouderling}, e-mail: #{email}"
 end
