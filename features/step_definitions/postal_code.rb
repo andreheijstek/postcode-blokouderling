bf90a118c181c41fa158f8019584e30d9275da0c
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-04-19 07:57:27 +0200

    Remove unnessesary find statement

--- features/step_definitions/postal_code.rb
+++ features/step_definitions/postal_code.rb
@@ -11,10 +11,11 @@ When(/^I search for my blokouderling$/) do
 end
 
 Then(/^the "([^"]*)" and "([^"]*)" should be shown$/) do |blokouderling, email|
-  element = find(:id, 'blokouderling_id')
   expect(page).to have_content "Ouderling: #{blokouderling}, e-mail: #{email}"
 end
 
 Then(/^this message should be shown: "([^"]*)"$/) do |message|
   expect(page).to have_content message
 end
+
+# Repeat for http://sintjansgemeente.nl/contact/inschrijven/
--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,10 @@
 require 'capybara/cucumber'
 require 'capybara/session'
 require 'capybara'
+require 'capybara/poltergeist'
 
-Capybara.default_driver        = :webkit
+Capybara.javascript_driver     = :poltergeist
+Capybara.default_driver        = :poltergeist
 Capybara.run_server            = false
 Capybara.default_selector      = :css
 Capybara.default_max_wait_time = 10
